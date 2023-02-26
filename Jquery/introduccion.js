70c350ebd90deef515d811a20343c4c7bfb70f63
emanuelplaceriani <emanuelplaceriani@gmail.com>
2023-02-26 20:34:56 -0300

    Initial commit

---
+++ Jquery/introduccion.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	/*$(selector).accion();
+	SELECTOR - Accedemoa al elemento que queremos trabajar
+	ACCION - Lo que queremos hacer con el elemento relacionado
+	*/
+
+	$('h1').hide();
+
+});
+
+/*Esto es lo mismo que
+ $(document).ready(function(){
+	
+});*/
+
+$(function(){
+	//bloque de codigo
+});
---
+++ Jquery/metodosDom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	/*$('#segunda').prevAll().slideDown(3000); aplica efecto deslizado hacia arriba de todos los previos a "segunda"*/ 
+	$(".padre").children().css(
+	{
+		background: #000000;
+		color: #ffffff;
+	})	
+})
